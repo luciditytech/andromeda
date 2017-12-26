f0c32ca772ba073f88f5e3fbfbeb39c7509a0f0f
Miguel Morales <therevoltingx@gmail.com>
2017-12-26 14:28:01 -0800

    cleans up sign up flow

--- contracts/Registrations.sol
+++ contracts/Registrations.sol
@@ -6,49 +6,56 @@ import "token-sale-contracts/contracts/HumanStandardToken.sol";
 
 contract Registrations is Ownable {
   address public tokenAddress = 0x1588d300a9995934aa9daae19be285a66eb46c1c;
-  uint256 minimum = 1000 * 1000000000;
 
   struct Verifier {
     address id;
-    string fqdn;
+    string location;
+    bool active;
+    bool created;
     uint256 balance;
   }
 
   mapping(address => Verifier) public verifiers;
   address[] public addresses;
 
-  function create(string _fqdn) {
-    Token token = Token(tokenAddress);
-    uint256 tokensOwned = token.balanceOf(msg.sender);
-    require(tokensOwned > 0);
-    require(verifiers[msg.sender].balance == 0);
-    var approved = token.allowance(msg.sender, this);
-    require(approved >= minimum);
-    var res = token.transferFrom(msg.sender, this, approved);
-    require(res == true);
-    var verifier = Verifier(msg.sender, _fqdn, approved);
-    verifiers[msg.sender] = verifier;
+  function create(string _location, bool _active) {
+    require(!verifiers[msg.sender].created);
+    verifiers[msg.sender].id = msg.sender;
+    verifiers[msg.sender].location = _location;
+    verifiers[msg.sender].active = _active;
     addresses.push(msg.sender);
   }
 
-  function update(string _fqdn) {
-    var verifier = verifiers[msg.sender];
-    verifier.fqdn = _fqdn;
-    verifiers[msg.sender] = verifier;
-  }
-
-  function getTokenAllowance() public constant returns (uint256) {
-    Token token = Token(tokenAddress);
-    return token.allowance(msg.sender, this);
-  }
-
-  function getBalanceOf(address _address) public constant returns (uint256) {
-    var verifier = verifiers[_address];
-    return verifier.balance;
+  function update(string _location, bool _active) {
+    require(!verifiers[msg.sender].created);
+    verifiers[msg.sender].location = _location;
+    verifiers[msg.sender].active = _active;
   }
 
   function getNumberOfVerifiers() public constant returns (uint) {
     return addresses.length;
+  }
+
+  function withdraw(uint256 _value) returns (bool success) {
+    var total = verifiers[msg.sender].balance;
+    var newTotal = total - _value;
+    require(newTotal >= 0);
+    verifiers[msg.sender].balance = newTotal;
+    Token token = Token(tokenAddress);
+    var res = token.transfer(msg.sender, _value);
+    if (!res) { revert(); }
+    return res;
+  }
+
+  /* erc20 approval callback function */
+  function receiveApproval(address _from, uint256 _value, address _token, bytes _data) returns (bool success) {
+    Token token = Token(tokenAddress);
+    var approved = token.allowance(_from, this);
+    require(approved > 0);
+    var res = token.transferFrom(_from, this, approved);
+    if (!res) { revert(); }
+    verifiers[_from].balance += approved;
+    return true;
   }
 
   /* owner only functions */
@@ -56,28 +63,6 @@ contract Registrations is Ownable {
     tokenAddress = _newTokenAddress;
   }
 
-  function changeMinimum(uint256 _newMinimum) onlyOwner {
-    minimum = _newMinimum;
-  }
-
-  function remove(address _address) onlyOwner {
-    Token token = Token(tokenAddress);
-    var verifier = verifiers[_address];
-    bool res = token.approve(_address, verifier.balance);
-    if (!res) { revert(); }
-
-    bool found = false;
-
-    for (uint256 i = 0; i < addresses.length; i++) {
-      var item = addresses[i];
-      if (item == _address) { 
-        delete addresses[i];
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) { revert(); }
-    delete verifiers[msg.sender];
-  }
+  /* disable default methods */
+  function() {throw;}
 }
