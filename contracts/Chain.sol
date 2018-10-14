f77d414e3d553084aa4241a2a52ed28bbcaf359e
Miguel Morales <therevoltingx@gmail.com>
2018-10-13 23:36:48 -0700

    chore(registry): updates location of registry contracts

--- contracts/Chain.sol
+++ contracts/Chain.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.4.24;
 
 import "zeppelin-solidity/contracts/ReentrancyGuard.sol";
-import "pokedex/contracts/VerifierRegistry.sol";
+import "digivice/contracts/VerifierRegistry.sol";
+import "./interface/IChain.sol";
 import "./ChainConfig.sol";
 
 
